cf1a67010d342ad5f2cae9d406d951850247bfe8
nce27 <nc3elliott@gmail.com>
2023-05-30 14:21:42 +0100

    Created an introductory project with relevant add ons and gems.

---
+++ hello_web_project/app.rb
@@ -0,0 +1,12 @@
+#THIS IS THE MAIN FILE OF THE APPLICATION 
+
+require 'sinatra/base'
+require 'sinatra/reloader'
+
+class Application < Sinatra::Base
+  # This allows the app code to refresh
+  # without having to restart the server.
+  configure :development do
+    register Sinatra::Reloader
+  end
+end
