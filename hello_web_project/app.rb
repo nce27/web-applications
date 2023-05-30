fb6bd8684f3b50250b80574a8b91599453c1f02d
nce27 <nc3elliott@gmail.com>
2023-05-30 15:20:05 +0100

    Added some commented examples explaining routes and requests.

--- hello_web_project/app.rb
+++ hello_web_project/app.rb
@@ -10,3 +10,29 @@ class Application < Sinatra::Base
     register Sinatra::Reloader
   end
 end
+
+
+# Route examples:
+
+    # Incoming request: GET /todos/1
+
+    # Synatra decides which code to execute based on the incoming http request
+
+      # GET /   => execute some code
+
+      # GET /todos/1  => execute different code (*it will execute this one)
+
+      # POST /todos/2  => executes different code
+
+
+
+    # Incoming request: GET /posts
+
+    # Synatra checks each of the routes below if it matches the request
+    # since itsn't present it will return a 404 as the route hasnt been found/ matched
+
+      # GET /   => execute some code
+
+      # GET /todos/1  => execute different code (*it will execute this one)
+
+      # POST /todos/2  => executes different code
