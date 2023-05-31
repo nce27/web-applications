b7c92735981954626544bbd8d6c4b5e83040eade
nce27 <nc3elliott@gmail.com>
2023-05-31 10:44:08 +0100

    Added code for a get request for a name.

--- hello_web_project/app.rb
+++ hello_web_project/app.rb
@@ -6,9 +6,29 @@ require 'sinatra/reloader'
 class Application < Sinatra::Base
   # This allows the app code to refresh
   # without having to restart the server.
-  configure :development do
-    register Sinatra::Reloader
-  end
+
+
+  get '/hello' do
+    name = params[:name]
+
+    return "Hello #{name}"
+  end 
+
+
+
+
+  # get '/' do
+  #   return "Hello!"
+  # end
+
+  # get '/posts' do
+  #   return "A list of posts"
+  # end
+
+
+  # configure :development do
+  #   register Sinatra::Reloader
+  # end
 end
 
 
@@ -16,7 +36,7 @@ end
 
     # Incoming request: GET /todos/1
 
-    # Synatra decides which code to execute based on the incoming http request
+    # Sinatra decides which code to execute based on the incoming http request
 
       # GET /   => execute some code
 
@@ -28,7 +48,7 @@ end
 
     # Incoming request: GET /posts
 
-    # Synatra checks each of the routes below if it matches the request
+    # Sinatra checks each of the routes below if it matches the request
     # since itsn't present it will return a 404 as the route hasnt been found/ matched
 
       # GET /   => execute some code
